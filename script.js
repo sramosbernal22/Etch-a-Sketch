e2f67d42b37bf32f447acb42ade618b2b2d2b16c
sramosbernal22 <sramosbernal22@gmail.com>
2024-08-03 17:27:50 -0500

    Refactored code to have grid as function and now button feature works.

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    //Render 16 by 16 once DOM is loaded
-    const container = document.querySelector(".container");
 
-    for (let i = 0; i < 16; i++)
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const container = document.querySelector(".container");
+    function createGrid(size, container)
     {
-        let row = document.createElement("div");
-        row.classList.add("row");
-        container.appendChild(row);
-        for (let i = 0; i < 16; i++)
-        {
-            let box = document.createElement("div");
-            box.classList.add("box");
-            row.appendChild(box);
-        }
+        container.innerHTML = ''
+        for (let i = 0; i < size; i++)
+            {
+                let row = document.createElement("div");
+                row.classList.add("row");
+                container.appendChild(row);
+                for (let i = 0; i < size; i++)
+                {
+                    let box = document.createElement("div");
+                    box.classList.add("box");
+                    row.appendChild(box);
+                }
+            } 
     }
 
+    //Original 16 by 16
+    createGrid(16, container);
+   
     //Button Dimensions Section
     const button = document.querySelector(".dimensions");
     button.addEventListener("click", () =>{
-        alert("Hello");
-
+        let size = prompt("Input size dimensions");
+        createGrid(size, container);
     });
+
+
 
 
 
